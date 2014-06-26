80e31bce2dd06965f947d0c930b28b573d92ea01
drandx <garjuanpablo@gmail.com>
2014-06-26 01:47:00 -0500

    Initial check-in of module ClickNDone

---
+++ ClickNDone.Core/DataModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace XamChat.Core
+{
+	public class Conversation
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Username { get; set; }
+	}
+
+	public class Product
+	{
+		public int Id { get; set; } //Just a numeric identifier
+		public string Name { get; set; } //Name of the product
+		public float Price { get; set; } //Price of the product
+		public Product ()
+		{
+		}
+	}
+
+	public class Message
+	{
+		public int Id { get; set; }
+		public int ConversationId { get; set; }
+		public int UserId { get; set; }
+		public string Username { get; set; }
+		public string Text { get; set; }
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+
+}
+
---
+++ ClickNDone.Core/ISettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace XamChat.Core
+{
+	public interface ISettings
+	{
+		User User { get; set; }
+		void Save();
+	}
+}
+
