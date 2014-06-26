f02733d0bbeb4e521c0d4ebcafd3a2e022b866e6
drandx <garjuanpablo@gmail.com>
2014-06-26 17:35:14 -0500

    Modified folder structures in ClicNDone.Core

--- ClickNDone.iOS/Controllers/LoginController.designer.cs
+++ ClickNDone.iOS/Controllers/LoginController.designer.cs
@@ -1,12 +1,10 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
-using System;
 using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace ClickNDone.iOS
@@ -15,22 +13,29 @@ namespace ClickNDone.iOS
 	partial class LoginController
 	{
 		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		UITextField txtEmail { get; set; }
+		MonoTouch.UIKit.UIButton btnLogIn { get; set; }
 
 		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		UITextField txtPassword { get; set; }
+		MonoTouch.UIKit.UITextField txtEmail { get; set; }
 
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtPassword { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
 			if (txtEmail != null) {
 				txtEmail.Dispose ();
 				txtEmail = null;
 			}
+
 			if (txtPassword != null) {
 				txtPassword.Dispose ();
 				txtPassword = null;
+			}
+
+			if (btnLogIn != null) {
+				btnLogIn.Dispose ();
+				btnLogIn = null;
 			}
 		}
 	}
