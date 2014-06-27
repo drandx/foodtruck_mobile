8fb98298435fca055b7b5573dc4e2f461c1b858f
drandx <garjuanpablo@gmail.com>
2014-06-27 00:18:04 -0500

    Added login logic with fake webservice.

--- ClickNDone.iOS/Controllers/LoginController.cs
+++ ClickNDone.iOS/Controllers/LoginController.cs
@@ -4,13 +4,56 @@ using System;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using ClickNDone.Core;
 
 namespace ClickNDone.iOS
 {
 	public partial class LoginController : UIViewController
 	{
+		readonly LoginViewModel loginViewModel = (LoginViewModel)DependencyInjectionWrapper.Instance.ServiceContainer ().GetService (typeof(LoginViewModel));
+	
 		public LoginController (IntPtr handle) : base (handle)
 		{
+
 		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			btnLogIn.TouchUpInside += async(sender, e) =>
+			{
+				loginViewModel.Username = txtEmail.Text;
+				loginViewModel.Password = txtPassword.Text;
+				try {
+					await loginViewModel.Login();
+					PerformSegue("OnLogin", this);
+				}
+				catch (Exception exc)
+				{
+					new UIAlertView("Oops!", exc.Message, null, "Ok").Show();
+				}
+			};
+		}
+			
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(false);
+			loginViewModel.IsBusyChanged += OnIsBusyChanged;
+		}
+		public override void ViewWillDisappear(bool animated)
+		{
+			base.ViewWillDisappear(false);
+			loginViewModel.IsBusyChanged -= OnIsBusyChanged;
+		}
+
+		void OnIsBusyChanged(object sender, EventArgs e)
+		{
+			txtEmail.Enabled = 
+			txtPassword.Enabled =
+					btnLogIn.Enabled =
+						indicator.Hidden = !loginViewModel.IsBusy;
+		}
+
+
 	}
 }
--- ClickNDone.iOS/Controllers/LoginController.designer.cs
+++ ClickNDone.iOS/Controllers/LoginController.designer.cs
@@ -1,10 +1,12 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
+using System;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace ClickNDone.iOS
@@ -13,29 +15,38 @@ namespace ClickNDone.iOS
 	partial class LoginController
 	{
 		[Outlet]
-		MonoTouch.UIKit.UIButton btnLogIn { get; set; }
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton btnLogIn { get; set; }
 
 		[Outlet]
-		MonoTouch.UIKit.UITextField txtEmail { get; set; }
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIActivityIndicatorView indicator { get; set; }
 
 		[Outlet]
-		MonoTouch.UIKit.UITextField txtPassword { get; set; }
-		
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtEmail { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField txtPassword { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnLogIn != null) {
+				btnLogIn.Dispose ();
+				btnLogIn = null;
+			}
+			if (indicator != null) {
+				indicator.Dispose ();
+				indicator = null;
+			}
 			if (txtEmail != null) {
 				txtEmail.Dispose ();
 				txtEmail = null;
 			}
-
 			if (txtPassword != null) {
 				txtPassword.Dispose ();
 				txtPassword = null;
-			}
-
-			if (btnLogIn != null) {
-				btnLogIn.Dispose ();
-				btnLogIn = null;
 			}
 		}
 	}
