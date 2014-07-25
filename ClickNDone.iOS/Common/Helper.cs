5299931e3423e15840c27f624669c998e61d6c6b
drandx <garjuanpablo@gmail.com>
2014-07-25 00:15:03 -0500

    Changed footer.

--- ClickNDone.iOS/Common/Helper.cs
+++ ClickNDone.iOS/Common/Helper.cs
@@ -13,9 +13,10 @@ namespace ClickNDone.iOS
 
 		public static void setAppearances()
 		{
-			UIImage barImage = UIImage.FromFile("images/header2.png");
+			UIImage barImage = UIImage.FromFile("images/header-3-64.png");
 			UIImage barToolImage = UIImage.FromFile("images/barra_copyright@2x.png");
 			UINavigationBar.Appearance.SetBackgroundImage (barImage,UIBarMetrics.Default);
+
 
 			UIToolbar.Appearance.SetBackgroundImage (barToolImage, UIToolbarPosition.Bottom, UIBarMetrics.Default);
 		}
