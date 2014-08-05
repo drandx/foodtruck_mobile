00c58a28dbe3c37d8a305f8a3efbc6fa69da5cd3
drandx <garjuanpablo@gmail.com>
2014-08-04 20:06:29 -0500

    Added labels to the categories screen,

---
+++ ClickNDone.iOS/Common/MyUIButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace ClickNDone.iOS
+{
+	[MonoTouch.Foundation.Register ("MyUIButton")]
+	public class MyUIButton : UIButton
+	{
+		public MyUIButton (IntPtr handle) : base (handle)
+		{
+
+		}
+
+		public override void Draw(RectangleF rect)
+		{
+			UIColor borderColor = UIColor.FromRGB (0,167,229);
+			UIColor ButtonBackgroundColor = UIColor.White;
+
+			try
+			{
+				var path = UIBezierPath.FromRect(rect);
+
+				ButtonBackgroundColor.SetFill();
+				path.Fill();
+
+				CurrentTitleColor.SetStroke();
+				path.Stroke();
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error in SevenButton > Draw : {0}\n",ex.Message);
+			}
+
+			this.Layer.BorderColor = borderColor.CGColor;
+			this.Layer.BorderWidth = 2f;
+		}
+
+	}
+}
+
