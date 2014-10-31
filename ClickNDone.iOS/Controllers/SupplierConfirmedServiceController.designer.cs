5ef0dadc55c9e38aba91d9f124b444ebbd505b3f
drandx <garjuanpablo@gmail.com>
2014-10-31 06:52:31 -0500

    Working again without issues.

--- ClickNDone.iOS/Controllers/SupplierConfirmedServiceController.designer.cs
+++ ClickNDone.iOS/Controllers/SupplierConfirmedServiceController.designer.cs
@@ -12,9 +12,63 @@ namespace ClickNDone.iOS
 	[Register ("SupplierConfirmedServiceController")]
 	partial class SupplierConfirmedServiceController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtAddress { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtDate { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtReference { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtState { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtTime { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtUserLastName { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtUserName { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (txtUserName != null) {
+				txtUserName.Dispose ();
+				txtUserName = null;
+			}
+
+			if (txtUserLastName != null) {
+				txtUserLastName.Dispose ();
+				txtUserLastName = null;
+			}
+
+			if (txtDate != null) {
+				txtDate.Dispose ();
+				txtDate = null;
+			}
+
+			if (txtTime != null) {
+				txtTime.Dispose ();
+				txtTime = null;
+			}
+
+			if (txtAddress != null) {
+				txtAddress.Dispose ();
+				txtAddress = null;
+			}
+
+			if (txtReference != null) {
+				txtReference.Dispose ();
+				txtReference = null;
+			}
+
+			if (txtState != null) {
+				txtState.Dispose ();
+				txtState = null;
+			}
 		}
 	}
 }
